f0202ab85aa8df3fb036f8ca88a3af739db9b197
Nils Henrich <nilshenrich@web.de>
2020-12-16 08:58:56 +0100

    Added install rule to CMakeLists.txt

--- compileTest.sh
+++ compileTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -std=c++17 -c ds18b20_test.cpp -I./include/ -o build/ds18b20_test.o
-g++ -o build/DS18B20 build/ds18b20_test.o -L./build -lds18b20 -lbcm2835 -lstdc++fs
+g++ -std=c++17 -c ds18b20_test.cpp -I/usr/local/include -o build/ds18b20_test.o
+g++ -o build/DS18B20 build/ds18b20_test.o -L/usr/local/lib -lds18b20 -lbcm2835 -lstdc++fs
